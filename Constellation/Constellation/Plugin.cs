311aac402014e9ae63be4f85abfe912d23868d9a
Erwann Laville <hydro@furya.ovh>
2017-06-17 14:18:50 +0200

    Constellation

---
+++ Constellation/Constellation/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Plugins.PushBulletNotifications.Configuration;
+
+namespace MediaBrowser.Plugins.PushBulletNotifications
+{
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public override string Name
+        {
+            get { return "PushBullet Notifications"; }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "Sends notifications via PushBullet Service.";
+            }
+        }
+
+        public static Plugin Instance { get; private set; }
+    }
+}
